9c9840992ec80464f26a5c4edac6fa4f6ad33759
maherrera603 <maherrera603@misena.edu.co>
2024-12-31 20:20:39 -0500

    connection to mongodb realized

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("init proyect")
+import { envs } from "./config";
+import { MongoDatabase } from "./data";
+import { AppRoutes, Server } from "./presentation"
+
+( async () => {
+    main()
+})()
+
+
+async function main(){
+
+    await MongoDatabase.connect({
+        mongoUrl: envs.MONGO_URL,
+        dbName: envs.MONGO_DB
+    });
+
+    const server = new Server({
+        port: envs.PORT,
+        routes: AppRoutes.routes
+    });
+
+    server.start();
+}
